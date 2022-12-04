9bf202ea288bb026ac9a282abcdc381cac72fa89
mohamedelshamy6 <elshamy.mohamed13@yahoo.com>
2022-12-04 20:42:00 +0200

    update

--- lib/ui/Screens/login.dart
+++ lib/ui/Screens/login.dart
@@ -30,178 +30,237 @@ class _LoginState extends State<Login> {
       ),
       borderRadius: BorderRadius.circular(15),
     );
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passController = TextEditingController();
+    String? email, password;
+    var formKey = GlobalKey<FormState>();
+
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Login to Dr.Sameh',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+      body: Container(
+        padding: const EdgeInsetsDirectional.all(15),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            const Color(0xff1E3163).withOpacity(0.85),
+            const Color(0xff2D46B9).withOpacity(0.85)
+          ], stops: const [
+            0.2,
+            0.8
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsetsDirectional.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadiusDirectional.circular(25),
                   ),
-                ),
-                const SizedBox(height: 50),
-                const Text(
-                  'Email',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(
-                      Icons.alternate_email,
-                      color: Colors.black,
-                    ),
-                    enabledBorder: borderStyle,
-                    hintText: 'zzzzz@gmail.com',
-                    border: borderStyle,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                const Text(
-                  'Password',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                TextFormField(
-                  controller: passController,
-                  obscureText: !isVisible!,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(
-                      Icons.key,
-                      color: Colors.black,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                          isVisible! ? Icons.visibility : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          isVisible = !isVisible!;
-                        });
-                      },
-                      color: Colors.black,
-                    ),
-                    enabledBorder: borderStyle,
-                    hintText: '********',
-                    border: borderStyle,
-                  ),
-                ),
-                const SizedBox(height: 50),
-                Center(
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: MaterialButton(
-                      color: Colors.blue[800],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.85,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Login to Dr.Sameh',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      onPressed: () {
-                        auth
-                            .signInWithEmailAndPassword(
-                                email: emailController.text,
-                                password: passController.text)
-                            .then((value) {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const HomeScreen(),
-                              ));
-                        });
-                      },
-                      child: const Text(
-                        'Login',
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: Center(
+                          child: Image.asset('images/login.jpg'),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Email',
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 75),
-                Row(
-                  children: const [
-                    Expanded(
-                      child: Divider(
-                        thickness: 2,
-                        color: Colors.black,
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Email must be full';
+                          }
+                          if (value.contains('@') == false) {
+                            return 'Email must have @';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.alternate_email,
+                            color: Colors.black,
+                          ),
+                          enabledBorder: borderStyle,
+                          errorBorder: borderStyle,
+                          hintText: 'zzzzz@gmail.com',
+                          border: borderStyle,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        'Or continue with',
+                      const SizedBox(height: 25),
+                      const Text(
+                        'Password',
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 24,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 2,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 75),
-                Center(
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: MaterialButton(
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                          width: 1,
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      onPressed: () {
-                        signInWithGoogle()
-                            .then((value) => Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const HomeScreen(),
-                                )));
-                      },
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 50,
-                            child: Image.asset('images/google.png'),
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password must be full';
+                          }
+                          if (value.length < 8) {
+                            return 'Password must be at least 8 letters or numbers';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        obscureText: !isVisible!,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.key,
+                            color: Colors.black,
                           ),
-                          const SizedBox(width: 75),
-                          const Text(
-                            'Google',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
+                          suffixIcon: IconButton(
+                            icon: Icon(isVisible!
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                isVisible = !isVisible!;
+                              });
+                            },
+                            color: Colors.black,
+                          ),
+                          enabledBorder: borderStyle,
+                          errorBorder: borderStyle,
+                          hintText: '********',
+                          border: borderStyle,
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      Center(
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: MaterialButton(
+                            color: Colors.blue[800],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
                             ),
-                          )
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {}
+                              auth
+                                  .signInWithEmailAndPassword(
+                                      email: email!, password: password!)
+                                  .then((value) {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const HomeScreen(),
+                                    ));
+                              });
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: const [
+                          Expanded(
+                            child: Divider(
+                              thickness: 2,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'Or continue with',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              thickness: 2,
+                              color: Colors.black,
+                            ),
+                          ),
                         ],
                       ),
-                    ),
+                      const SizedBox(height: 15),
+                      Center(
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: MaterialButton(
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                width: 1,
+                                color: Colors.black,
+                              ),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            onPressed: () {
+                              signInWithGoogle()
+                                  .then((value) => Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const HomeScreen(),
+                                      )));
+                            },
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 50,
+                                  child: Image.asset('images/google.png'),
+                                ),
+                                const SizedBox(width: 75),
+                                const Text(
+                                  'Google',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 75),
-              ],
+              ),
             ),
           ),
         ),
