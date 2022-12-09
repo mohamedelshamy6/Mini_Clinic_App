4ef675046ea53c0a230b8d9c74f38b4eab580a63
mohamedelshamy6 <elshamy.mohamed13@yahoo.com>
2022-12-09 17:31:50 +0200

    update

--- lib/ui/Screens/home_screen.dart
+++ lib/ui/Screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mini_hospital/ui/Screens/login.dart';
 import 'package:mini_hospital/ui/Screens/reservation.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,7 +9,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GoogleSignIn user = GoogleSignIn();
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white24,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: const Color(0xff1E3163).withOpacity(0.85),
+            ),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              user.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Login(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: SizedBox(
           height: 400,
@@ -58,37 +84,36 @@ class HomeScreen extends StatelessWidget {
                 const SizedBox(height: 25),
                 Expanded(
                   child: Card(
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusDirectional.circular(15),
-                    ),
-                    child:Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        gradient: LinearGradient(
-                            colors: [
-                              const Color(0xff1E3163).withOpacity(0.85),
-                              const Color(0xff2D46B9).withOpacity(0.85)
-                            ],
-                            stops: const [
-                              0.2,
-                              0.8
-                            ],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight),
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(15),
                       ),
-                      child: const Center(
-                        child: Text(
-                          'Go to chat screen',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 26,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          gradient: LinearGradient(
+                              colors: [
+                                const Color(0xff1E3163).withOpacity(0.85),
+                                const Color(0xff2D46B9).withOpacity(0.85)
+                              ],
+                              stops: const [
+                                0.2,
+                                0.8
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight),
                         ),
-                      ),
-                    )
-                  ),
+                        child: const Center(
+                          child: Text(
+                            'Go to chat screen',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      )),
                 ),
               ],
             ),
--- lib/ui/Screens/splash_screen.dart
+++ lib/ui/Screens/splash_screen.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:mini_hospital/services/notification_services.dart';
+import 'package:mini_hospital/ui/Screens/home_screen.dart';
 import 'package:mini_hospital/ui/Screens/login.dart';
+import 'package:mini_hospital/view_models/patients_view_model.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,21 +16,52 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
+List dates = [];
+
 class _SplashScreenState extends State<SplashScreen> {
+  late StreamSubscription<User?> user;
+
   @override
   void initState() {
+    allNotification();
+    user = FirebaseAuth.instance.authStateChanges().listen((user) {});
     Timer(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => const Login(),
+            builder: (context) => FirebaseAuth.instance.currentUser == null
+                ? const Login()
+                : const HomeScreen(),
           ));
     });
     super.initState();
   }
 
   @override
+  void dispose() {
+    user.cancel();
+    super.dispose();
+  }
+
+  allNotification() {
+    for (Timestamp i in dates) {
+      DateTime date = i.toDate().subtract(const Duration(minutes: 15));
+      NotificationApi().showScheduledNotification(
+          scheduleDate: date,
+          title: 'Patient reminder',
+          body: 'You have an appointment in 15 minutes');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<PatientsViewModel>();
+    final allDates = viewModel.patient ?? [];
+    for (int i = 0; i < allDates.length; i++) {
+      setState(() {
+        dates.add(allDates[i].fullDate);
+      });
+    }
     return Scaffold(
       body: SafeArea(
         child: Padding(
